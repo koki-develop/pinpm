a2d227e304e5b1857335e059d613a71b21ac17ac
koki-develop <kou.pg.0131@gmail.com>
2025-02-16 08:38:40 +0900

    fix: Use process.stderr for spinner output

--- src/lib/spinner.ts
+++ src/lib/spinner.ts
@@ -9,20 +9,20 @@ export const withSpinner = async <T>(
 ): Promise<T> => {
   let i = 0;
   const interval = setInterval(() => {
-    process.stdout.write(`\r\x1b[36m${spinner[i]}\x1b[0m ${message.start}`);
+    process.stderr.write(`\r\x1b[36m${spinner[i]}\x1b[0m ${message.start}`);
     i = (i + 1) % spinner.length;
   }, 75);
 
   return fn()
     .then((result) => {
-      process.stdout.write(`\r\x1b[32m\x1b[1m✓\x1b[0m ${message.start}\n`);
+      process.stderr.write(`\r\x1b[32m\x1b[1m✓\x1b[0m ${message.start}\n`);
       if (message.result) {
-        process.stdout.write(`  └─ ${message.result(result)}\n`);
+        process.stderr.write(`  └─ ${message.result(result)}\n`);
       }
       return result;
     })
     .catch((error) => {
-      process.stdout.write(`\r\x1b[31m\x1b[1m✗\x1b[0m ${message.start}\n`);
+      process.stderr.write(`\r\x1b[31m\x1b[1m✗\x1b[0m ${message.start}\n`);
       throw error;
     })
     .finally(() => {
