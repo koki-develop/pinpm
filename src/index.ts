27ff7495d8ed286f8772a5a31642d5e24d950fcf
koki-develop <kou.pg.0131@gmail.com>
2025-02-16 04:45:12 +0900

    Add version to CLI using package.json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { Command } from "commander";
 import { type Options, main } from "./main";
+import { version } from "../package.json";
 
 const program = new Command();
 
-// TODO: version
-
 program
+  .name("pinpm")
+  .version(version)
   .option("-l, --lockfile <lockfile>", "lockfile to use")
   .parse(process.argv);
 
