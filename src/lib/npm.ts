e4c37601792fc01938eb8ad8df8f90fe40b753ac
koki-develop <kou.pg.0131@gmail.com>
2025-02-16 03:39:11 +0900

    Add npm utility function to list package versions

---
+++ src/lib/npm.ts
@@ -0,0 +1,20 @@
+import { sh } from "./sh";
+
+export const npmList = async (): Promise<Record<string, string>> => {
+  const stdout = await sh("npm", [
+    "list",
+    "--package-lock-only",
+    "--json",
+    "--depth=0",
+  ]);
+
+  const parsed = JSON.parse(stdout);
+
+  return Object.entries(parsed.dependencies).reduce(
+    (acc, [name, info]) => {
+      acc[name] = (info as { version: string }).version;
+      return acc;
+    },
+    {} as Record<string, string>,
+  );
+};
