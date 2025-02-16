ef8d20877e0e258d8dbcd03c29d20464b08801c3
koki-develop <kou.pg.0131@gmail.com>
2025-02-16 15:32:33 +0900

    feat: Add Color utility for terminal text styling

--- src/lib/spinner.ts
+++ src/lib/spinner.ts
@@ -1,3 +1,5 @@
+import { Color } from "./color";
+
 const spinner = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
 
 export const withSpinner = async <T>(
@@ -9,13 +11,15 @@ export const withSpinner = async <T>(
 ): Promise<T> => {
   let i = 0;
   const interval = setInterval(() => {
-    process.stderr.write(`\r\x1b[36m${spinner[i]}\x1b[0m ${message.start}`);
+    process.stderr.write(`\r${new Color(spinner[i]).cyan()} ${message.start}`);
     i = (i + 1) % spinner.length;
   }, 75);
 
   return fn()
     .then((result) => {
-      process.stderr.write(`\r\x1b[32m\x1b[1m✓\x1b[0m ${message.start}\n`);
+      process.stderr.write(
+        `\r${new Color("✓").green().bold()} ${message.start}\n`,
+      );
       if (message.result) {
         const resultString = message.result(result);
         if (Array.isArray(resultString)) {
@@ -33,7 +37,9 @@ export const withSpinner = async <T>(
       return result;
     })
     .catch((error) => {
-      process.stderr.write(`\r\x1b[31m\x1b[1m✗\x1b[0m ${message.start}\n`);
+      process.stderr.write(
+        `\r${new Color("✗").red().bold()} ${message.start}\n`,
+      );
       throw error;
     })
     .finally(() => {
